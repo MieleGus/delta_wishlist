5a57bf9678f54291706c46f7feb336b716c75833
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-06-21 17:22:17 -0300

    finished card styles

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <>
+        <nav class="navbar navbar-dark bg-dark">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="navbar-brand" href="#">Produtos</a>
+                    <a class="navbar-brand" href="#">Lista de desejos</a>
+                </li>
+            </ul>
+            
+        </nav>
+         </>
+    )
+}
+
+export default Header
--- frontend/src/pages/Products/index.js
+++ frontend/src/pages/Products/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 // import './App.css';
 import api from '../../services/api'
 
+import Header from '../../components/Header'
+
 function Products() {
 const [products, setProducts] = useState([])
 
@@ -15,20 +17,21 @@ console.log(products)
 
   return ( 
     <>
+    <Header/>
     <div className="container">
-        <div className="row card-group">
+        <div className="row card-deck">
         {products.map(product => (
-            <div className="col-lg-3 col-md-6" key={product.rank}>
-                <div className="card mb-3 shadow-sm" style={{width: '100%'}}>
+            <div className="col-lg-3 col-md-6 mt-5" key={product.rank}>
+                <div className="card card3 mb-3 bg-light  shadow-sm" style={{width: '100%'}}>
                     <img className="card-img-top" src={product.image} style={{height: 200, width: '80%,'}}alt="Imagem de capa do card"/>
                     <div className="card-body">
                         <h5 className="card-title mb-3">{product.title}</h5>
                         <p className="card-text">{product.price_upper.raw}</p>
                         <div className="d-flex justify-content-between align-items-center">
-                            <div className="btn-group ">
-                                <button>Comprar</button>
-                                <button>Adicionar</button>
-                                <button>Remover</button>
+                            <div className="btn-group go-corner">
+                                <button className="btn btn-sm btn-outline-secondary">Comprar</button>
+                                <button className="btn btn-sm btn-outline-secondary">Adicionar</button>
+                                <button className="btn btn-sm btn-outline-secondary">Remover</button>
                             </div>
                         </div>
                     </div>
